d95759cff0a682c89a46346c6a9246068e8640f2
ShaliniLodhi <55933789+Shalini-lodhi@users.noreply.github.com>
2021-07-20 19:55:23 +0530

    Create Fibonacci_Number.cpp

---
+++ Algorithms/DynamicProgramming/Fibonacce/Fibonacci_Number.cpp
@@ -0,0 +1,13 @@
+int fib(int n) {
+        int f[n + 2];
+    int i;
+ 
+    f[0] = 0;
+    f[1] = 1;
+ 
+    for(i = 2; i <= n; i++)
+    {
+       f[i] = f[i - 1] + f[i - 2];
+    }
+    return f[n];
+    }
