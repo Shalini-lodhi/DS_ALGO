75c354e7bae329427c46d4ddac957888384f2dd9
ShaliniLodhi <55933789+Shalini-lodhi@users.noreply.github.com>
2021-07-23 23:33:50 +0530

    Create Palindromic Substrings.cpp

---
+++ Algorithms/DynamicProgramming/LCS-Longest_Common_Subsequence/Palindromic Substrings.cpp
@@ -0,0 +1,28 @@
+ int LCS(string s1, string s2, int n, int m){
+        int t[n+1][m+1];
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=m; j++){
+                if(i==0 || j==0){
+                    t[i][j]=0;
+                }
+            }
+        }int ans=0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s1[i-1]==s2[j-1]){
+                    t[i][j]=1+t[i-1][j-1];
+                    ans=max(ans, t[i][j]);
+                }
+                else{
+                    t[i][j]=0;
+                }
+            }
+        }
+        return ans;
+    }
+    int countSubstrings(string s){
+     string org_s=s;
+    reverse(s.begin(), s.end());
+        int sz=s.size();
+        return LCS(org_s, s, sz, sz);
+    }
