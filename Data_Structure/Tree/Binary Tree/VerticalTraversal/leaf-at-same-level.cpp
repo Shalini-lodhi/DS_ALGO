b8b22413cf4f312801d7afad3e377b3a103d537e
ShaliniLodhi <55933789+Shalini-lodhi@users.noreply.github.com>
2021-07-31 00:20:24 +0530

    leaf-at-same-level
    Input: 
                1
              /   \
             2     3
    
    Output: 1
    
    Explanation: 
    Leaves 2 and 3 are at same level.

---
+++ Data_Structure/Tree/Binary Tree/VerticalTraversal/leaf-at-same-level.cpp
@@ -0,0 +1,20 @@
+int maxi(Node* root)
+{
+if(!root) return 0;
+
+return max(maxi(root->left),maxi(root->right))+1;
+}
+
+int mini(Node* root)
+{
+if(!root) return INT_MAX;
+
+if(!root->left and !root->right) return 1;
+
+return min(mini(root->left),mini(root->right))+1;
+}
+bool check(Node *root)
+{
+//Your code here
+return maxi(root)==mini(root);
+}
