d783dc86ebca510fe17162043ef30c5c249b99e6
Shalini Lodhi <55933789+Shalini-lodhi@users.noreply.github.com>
2021-08-08 15:48:57 +0530

    median-of-bst
    Input:
           6
         /   \
       3      8   
     /  \    /  \
    1    4  7    9
    Output: 6
    Explanation: Inorder of Given BST will be:
    1, 3, 4, 6, 7, 8, 9. So, here median will 6.

---
+++ Data_Structure/Tree/BinarySearchTree/median-of-bst.cpp
@@ -0,0 +1,18 @@
+void inorder(Node *root, vector<int> &v){
+if(!root)
+return;
+inorder(root->left, v);
+v.push_back(root->data);
+inorder(root->right, v);
+}
+
+float findMedian(struct Node *root)
+{
+//Code here
+vector<int> v;
+inorder(root, v);
+if(v.size()%2 == 1)
+return v[v.size()/2];
+int mid = v.size()/2;
+return (v[mid] + v[mid-1])/2.0;
+}
